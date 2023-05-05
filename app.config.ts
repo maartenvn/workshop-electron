7f52a0d5ff5fa3485df6dd3e8c486ccf42aef124
maartenvn <maartenvn.eu@gmail.com>
2023-05-05 14:29:12 +0200

    Commit code to Github

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  docus: {
+    title: "Workshop Electron",
+    description: "Workshop voor het leren werken met Electron JS",
+    image: "https://www.electronjs.org/assets/img/logo.svg",
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: false,
+    },
+  },
+});
